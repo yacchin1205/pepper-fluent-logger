2faa1c2d567d92e6885b9ba388b6aaf3d6add17a
yacchin1205 <yazawa@yzwlab.net>
2015-10-30 13:24:35 +0900

    Add preference support

--- fluentlogger/fluentlogger.py
+++ fluentlogger/fluentlogger.py
@@ -6,8 +6,11 @@ import threading
 from fluent import sender
 from fluent import event
 
+PREF_DOMAIN = 'com.github.yacchin1205.fluentlogger'
+
 class FluentLoggerService:
-    def __init__(self):
+    def __init__(self, session):
+        self.session = session
         self.lock = threading.RLock()
         self.running = False
         
@@ -15,10 +18,15 @@ class FluentLoggerService:
         with self.lock:
             if self.running:
                 self.stop()
-            sender.setup('pepper', host='192.168.10.119', port=24224)
-            event.Event('service', {
-                'status': 'started'
-            })
+            host = self._get_pref('host')
+            if host is not None:
+                tag = self._get_pref('tag', 'pepper')
+                sender.setup(tag, host=host,
+                             port=int(self._get_pref('port', '24224')))
+                event.Event('service', {
+                    'status': 'started'
+                })
+                self.running = True
     
     def stop(self):
         with self.lock:
@@ -26,13 +34,27 @@ class FluentLoggerService:
                 event.Event('service', {
                     'status': 'stopped'
                 })
-            self.running = False    
+            self.running = False
+    
+    def setForwarder(self, host, port):
+        prefManager = self.session.service('ALPreferenceManager')
+        prefManager.setValue(PREF_DOMAIN, 'host', host)
+        prefManager.setValue(PREF_DOMAIN, 'port', str(port))
+        self.start()
+            
+    def _get_pref(self, name, default_value=None):
+        prefManager = self.session.service('ALPreferenceManager')
+        value = prefManager.getValue(PREF_DOMAIN, name)
+        if value is None:
+            return default_value
+        else:
+            return value
     
 def main():
     app = qi.Application()
     app.start()
     session = app.session
-    myService = FluentLoggerService()
+    myService = FluentLoggerService(session)
     session.registerService("FluentLoggerService", myService)
     myService.start()
     app.run()
